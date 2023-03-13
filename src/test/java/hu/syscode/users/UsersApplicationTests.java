6c5b49fa6f6604d04cd39cec8160074bd97b4f0d
Péter Attila Rigó <61515182+rigopeti96@users.noreply.github.com>
2023-03-13 19:56:26 +0100

    Initial commit

---
+++ src/test/java/hu/syscode/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.syscode.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
